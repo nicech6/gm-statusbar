e82def573c0491761f8d876eb4dab005f6b9d875
cuihai <cuihai_lte@163.com>
2017-04-27 23:10:51 +0800

    修改已知BUG

--- app/src/main/java/com/cuihai/gmstatusbar/MainActivity.java
+++ app/src/main/java/com/cuihai/gmstatusbar/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StatusBarUtils.compat(this,R.color.colorAccent);
     }
 }
